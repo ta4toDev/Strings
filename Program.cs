91766769b30a7af4dcaf1f82ad013498e449edfd
ta4toDev <irina.jaudszims@gmail.com>
2024-03-12 14:18:53 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarieren und Initialisieren der Variablen meinName
+            string meinName = "Irina";
+
+            string nachricht = "Mein Name ist " + meinName;
+            int laengeDesStrings = nachricht.Length;
+            // die ToUpper Methode gibt den String in Großschrift zurück.
+            string allesGrossGeschrieben = nachricht.ToUpper();
+            string allesKleinGeschrieben = nachricht.ToLower();
+            Console.WriteLine(allesGrossGeschrieben);
+            Console.WriteLine(allesKleinGeschrieben);
+            Console.WriteLine(nachricht + " ist {0} Zeichen {1} lang", laengeDesStrings);
+            Console.ReadKey();
+        }
+    }
+}
